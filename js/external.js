e75db99e442d3693f21b0af8cdc2600bf7763c22
Piotr Chomicki <pkc@Jans-Air.localdomain>
2022-07-28 14:03:40 -0500

    js with html notes

---
+++ js/external.js
@@ -0,0 +1,19 @@
+"use strict";
+var myAge;
+
+myAge = 25;
+
+console.log("this is how we do it");
+console.log("this is my second alert");
+alert("this is not a drill");
+alert("this is really not a drill");
+
+confirm("are you sure, well are you, punk!");
+
+var isOkay = confirm("click okay if you are okay");
+console.log(isOkay);
+
+var name = prompt("what is your name");
+
+console.log(name);
+
