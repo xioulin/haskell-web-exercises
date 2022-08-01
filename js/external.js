71de30b7d03e585ebf35821a62470e0d9544f893
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-08-01 11:34:57 -0500

    add functions

--- js/external.js
+++ js/external.js
@@ -1,19 +1,11 @@
 "use strict";
-var myAge;
 
-myAge = 25;
 
-console.log("this is how we do it");
-console.log("this is my second alert");
-alert("this is not a drill");
-alert("this is really not a drill");
+<script>
+    console.log("greetings from external JS!");
+    alert("Welcome to the Jungle!!");
+    let favcolor = prompt("what is ur fav color?");
+    alert("Great! only losers like the color "+favcolor);
 
-confirm("are you sure, well are you, punk!");
 
-var isOkay = confirm("click okay if you are okay");
-console.log(isOkay);
-
-var name = prompt("what is your name");
-
-console.log(name);
-
+</script>
