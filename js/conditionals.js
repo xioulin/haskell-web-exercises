7eacb1361c0f09261ead0c97f9e94e38813c1ce0
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-08-01 15:25:55 -0500

    h

--- js/conditionals.js
+++ js/conditionals.js
@@ -35,6 +35,6 @@ switch(pizzaPreference) {
         alert("Just plain cheese? Okay...");
         break;
     default:
-        alert(pizzaPreference + " isn't my favorite, but let's order some ok!");
+        alert(pizzaPreference + " isn't my favorite, but let's order some!");
         break;
 }
