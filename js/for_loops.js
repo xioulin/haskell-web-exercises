63fdcf208e4e4ee0de21bfe3a297dc533fab382a
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-08-03 11:10:31 -0500

    loop practice exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,16 +1,26 @@
-
-
-
+//Question 1.
 // var numb = prompt("enter number to get multiplication tables for this number")
-//
-//
-// function multiTables(for(numb, x=0; x <=10; x++ ){
-//     console.log(numb*i)
-// })
-//
-
-// var num = prompt("enter number")
-//
-// for(var x=0, num ; x <= 10; x*num){
-//     console.log(x);
+// function multiTable(){
+//     for(x=1; x <= 10; x++ ){
+//         var prod = numb*x;
+//         console.log(`${numb}*${x} is ${prod}`);
+//     }}
+// console.log("The multiplication tables for "+numb+" are:");
+// console.log(multiTable(numb));
+// Question 2.
+// for (var x=1; x <=10; x++){
+//     var randPaul = Math.floor(Math.random()*180)+20;
+//     console.log(x+" randomly generated number is "+randPaul);
+//     if (randPaul % 2 === 0 ){
+//         console.log(randPaul +" is even");
+//     }else if(randPaul %2 === 1){
+//         console.log(randPaul + " is odd");
+//     }
 // }
+
+//Question 2.
+for (x= 100; x == 0; x=x-5){
+    var x;
+    console.log(x);
+}
+
