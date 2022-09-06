aaf621abed0a9eaded779c34463997b8686608c9
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-09-06 09:05:32 -0500

    hello

--- js/j-query.js
+++ js/j-query.js
@@ -1,3 +1,10 @@
 $(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+$("li")
+
+var contents = $('#codeup').html();
+alert(contents);
+
+$('.important').css('background-color', 'yellow');
