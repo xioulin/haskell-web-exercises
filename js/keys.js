8aa3e26492e16d184e3fab3b00c33002010eafdd
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-09-12 13:52:15 -0500

    mapbox markers and popups

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,3 @@
-// mapbox_token = 'pk.eyJ1IjoianJlaWNoNSIsImEiOiJjanM0dmxlaGcwNTY0M3ltczN6eTd3MmZqIn0.81pMXjw5HunWwoiJWqSUGg'
+// MAPBOX_TOKEN = 'pk.eyJ1IjoianJlaWNoNSIsImEiOiJjanM0dmxlaGcwNTY0M3ltczN6eTd3MmZqIn0.81pMXjw5HunWwoiJWqSUGg'
+
+// MAPBOX_TOKEN = 'pk.eyJ1Ijoiam1hYzE5NjkiLCJhIjoiY2w3dXp0bmE1MDBzZzNwbXc4czMxemRtYyJ9.4yx3kRc85faI2unEMmbUSA'
