61ab578414161f398bd38e0f8fbc64f840aa24d8
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-09-13 10:47:27 -0500

    updating

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,4 @@
-// MAPBOX_TOKEN = 'pk.eyJ1IjoianJlaWNoNSIsImEiOiJjanM0dmxlaGcwNTY0M3ltczN6eTd3MmZqIn0.81pMXjw5HunWwoiJWqSUGg'
+// const MAPBOX_TOKEN = 'pk.eyJ1IjoianJlaWNoNSIsImEiOiJjanM0dmxlaGcwNTY0M3ltczN6eTd3MmZqIn0.81pMXjw5HunWwoiJWqSUGg'
 
-// MAPBOX_TOKEN = 'pk.eyJ1Ijoiam1hYzE5NjkiLCJhIjoiY2w3dXp0bmE1MDBzZzNwbXc4czMxemRtYyJ9.4yx3kRc85faI2unEMmbUSA'
+
+// const OPEN_WEATHER = 'f857a979b61a2dadff105d01e2afbe7e'
