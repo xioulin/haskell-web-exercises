4c3c9446d728341d5a7fb75462b4a10175266e53
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-11-13 20:55:45 -0600

    reworking iterating arrays

--- js/iterating.js
+++ js/iterating.js
@@ -7,6 +7,17 @@
      * 'names'.
      */
 
+    var names = ["ishmael","quequed","starbuck","ahab"];
+
+    names.forEach(function(name){
+        console.log(name);
+    })
+
+    for(var i = 0; i<names.length; i++){
+        console.log(names[i]+i);
+    }
+
+
 
 
     /**
@@ -14,8 +25,10 @@
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    //
-    // console.log("there are " + names.length+" elements in the array");
+   for(var i=0; i<names.length; i++){
+       console.log(names[i]);
+   }
+
 
     /**
      * TODO:
@@ -23,37 +36,55 @@
      * accessing each element's index.
      */
 
-    // console.log(names[0]);
-    // console.log(names[1]);
-    // console.log(names[2]);
-    // console.log(names[3]);
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
-    // for (var i = 0; i < names.length; i++) {
-    //     console.log('The name at index ' + i + ' is: ' + names[i]);
-    // }
+    for (var i = 0; i < names.length; i++) {
+        console.log('The name at index ' + i + ' is: ' + names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
 
-    // names.forEach(function(name,index) {
-    //     console.log('The name at index ' + index+' is '+ name);
-    // });
+    names.forEach(function(name,index) {
+        console.log('The name at index ' + index+' is '+ name);
+    });
 
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
      * return an an element from it
      * - first: returns the first item in the array
+
+    function returnFirst(array){
+        console.log(array[0]);
+    }
+    var arrayFirstLegalAge = [18,21,25];
+    console.log(returnFirst(arrayFirstLegalAge));
+
      * - second: returns the second item in the array
+     */
+     function returnSecond(array){
+             console.log(array[1]);
+         }
+    /**
      * - last: returns the last item in the array
-     *
+     */
+    function returnThird(array){
+        console.log(array[2]);
+    }
+    /**
+
+    /**
      * Example:
      *  > first([1, 2, 3, 4, 5]) // returns 1
      *  > second([1, 2, 3, 4, 5]) // returns 2
